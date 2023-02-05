5b6798a14be26d61ec406449331c73872b884381
Bryce Whitney <brycewhit13@gmail.com>
2023-02-05 18:41:20 -0500

    currency conversion cli

---
+++ currency-converter/src/lib.rs
@@ -0,0 +1,25 @@
+/* Takes 2 currency codes and prints out the exchange rates */
+use std::env;
+use std::fs;
+use reqwest;
+
+pub async fn get_conversion_rate(code1: &str, code2: &str){
+    // Get the API key
+    let key_path = env::current_dir().unwrap();
+    let key_path_inter = key_path.join("credentials.cfg");
+    let key_path_final = key_path_inter.to_str().unwrap();
+    
+    let file_string = fs::read_to_string(key_path_final).unwrap();
+    let start_pos = file_string.find("\"").unwrap() + 1;
+    let api_key = &file_string.trim()[start_pos..start_pos + 20];
+     
+    // Make the call to the API
+    let api_call_str: String = format!("https://free.currconv.com/api/v7/convert?q={code1}_{code2}&compact=ultra&apiKey={api_key}");
+    let result = reqwest::get(api_call_str).await.expect("REASON").text().await.unwrap();
+
+    // Format the results
+    let start_pos = result.find(":").unwrap() + 0; // Convert to int
+    let end_pos = start_pos + 6;
+    let final_results = &result[start_pos..end_pos];
+    println!("Conversion Rate from {code1} to {code2}: {:?}", final_results);
+}
---
+++ currency-converter/src/main.rs
@@ -0,0 +1,27 @@
+use tokio;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+//add extended help
+#[clap(
+    version = "4.1.4",
+    author = "Bryce Whitney",
+    about = "A command-line tool that looks at conversion rates between currencies",
+    after_help = "Example: cargo run -- "
+)]
+struct Args {
+    // First currency
+    #[arg(short, long)]
+    origin: String,
+ 
+    // Second currency
+    #[arg(short, long)]
+    destination: String,
+ }
+
+// Find a list of currency codes here: https://www.iban.com/currency-codes
+#[tokio::main]
+async fn main(){
+    let args = Args::parse();
+    currency_converter::get_conversion_rate(&args.origin, &args.destination).await;
+}
