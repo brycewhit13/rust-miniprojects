6fdf50cb9390f0862a0b39953c37ab7fca2f11ab
Bryce Whitney <brycewhit13@gmail.com>
2023-03-23 21:00:02 -0400

    added clap CLI functionality

--- hackernews-rs/src/lib.rs
+++ hackernews-rs/src/lib.rs
@@ -1,1 +1,34 @@
 // Import crates
+//use reqwest;
+//use serde;
+
+// Constants
+const BASE_URL: &str = "https://hacker-news.firebaseio.com/v0";
+
+// Enums
+pub enum QueryType {
+    TopStories,
+    BestStories,
+    NewStories,
+    JobStories,
+}
+
+// Functions
+// Make the proper request to the API
+pub async fn get_stories(query_type: QueryType) {
+    let url = format!("{}/{}", BASE_URL, get_extension(query_type));
+    
+    // TODO: Delete later after testing
+    println!("URL: {}", url);
+}
+
+// Get the extension depending on the QueryType
+// ? Do we need .json at the end of the extension?
+fn get_extension(query_type: QueryType) -> &'static str {
+    match query_type {
+        QueryType::TopStories => "topstories.json",
+        QueryType::BestStories => "beststories.json",
+        QueryType::NewStories => "newstories.json",
+        QueryType::JobStories => "jobstories.json",
+    }
+}
--- hackernews-rs/src/main.rs
+++ hackernews-rs/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+// Import crates
+use hackernews_rs::get_stories;
+use hackernews_rs::QueryType::{NewStories, TopStories,  BestStories, JobStories};
+use tokio;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+// add extended help
+// TODO: Add an example after the help
+#[clap(
+    version = "4.1.13",
+    author = "Bryce Whitney",
+    about = "A command-line tool that allows you to get stories from Hacker News",
+    after_help = "Example: cargo run -- -q best"
+)]
+
+// Create struct to hold command-line arguments
+struct Args {
+    // Create argument to hold sentence
+    #[clap(short, long, default_value = "new")]
+    query_type: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // Get command-line arguments
+    let args: Args = Args::parse();
+
+    // Get the query type
+    let query_type = match args.query_type.as_str() {
+        "top" => TopStories,
+        "best" => BestStories,
+        "new" => NewStories,
+        "job" => JobStories,
+        _ => NewStories,
+    };
+
+    // Get the stories
+    get_stories(query_type).await;
 }
